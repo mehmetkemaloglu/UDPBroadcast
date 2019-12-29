fcc0836785b257396dee06b06e8979ce340b713e
Mehmet Berk Kemaloğlu <berk.kemaloglu@boun.edu.tr>
2019-12-29 17:17:40 +0300

    First Commit

---
+++ broadcastClient/cmake-build-debug/CMakeFiles/broadcastClient.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/broadcastClient.dir/main.cpp.o"
+  "broadcastClient.pdb"
+  "broadcastClient"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/broadcastClient.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ broadcastServer/cmake-build-debug/CMakeFiles/broadcastServer.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/broadcastServer.dir/main.cpp.o"
+  "broadcastServer.pdb"
+  "broadcastServer"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/broadcastServer.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
